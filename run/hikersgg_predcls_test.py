a3768f60caf4c1261cc551efd9bf7016f7e14c75
Nyeilim <yan.jinghui@foxmail.com>
2024-09-20 21:56:23 +0800

    try origin val function whether it can run test normally

--- run/hikersgg_predcls_test.py
+++ run/hikersgg_predcls_test.py
@@ -10,7 +10,6 @@ from tqdm import tqdm
 from config import ModelConfig
 from lib.exp.exp_util import load_best_matrices
 from lib.pytorch_misc import optimistic_restore
-from lib.exp.val_fn import _val_epoch
 from lib.pytorch_misc import print_para
 from lib.my_model_24 import KERN
 from dataloaders.visual_genome import VGDataLoader, VG
@@ -87,10 +86,65 @@ detector = KERN(classes=train.ind_to_classes, rel_classes=train.ind_to_predicate
                 class_volume=1.0, with_clean_classifier=use_bpl, with_transfer=use_sa, sa=use_sa, config=conf,
                )
 
+from lib.evaluation.sg_eval import BasicSceneGraphEvaluator, calculate_mR_from_evaluator_list, eval_entry
+from config import BOX_SCALE, IM_SCALE
+from torch import no_grad as torch_no_grad
+from torch.cuda.amp import autocast
+def val_batch(batch_num, b, evaluator, evaluator_multiple_preds, evaluator_list, evaluator_multiple_preds_list):
+    with autocast():
+        det_res = detector[b]
+    if conf.num_gpus == 1:
+        det_res = [det_res]
+
+    for i, (boxes_i, objs_i, obj_scores_i, rels_i, pred_scores_i) in enumerate(det_res):
+        gt_entry = {
+            'gt_classes': val.gt_classes[batch_num + i].copy(),
+            'gt_relations': val.relationships[batch_num + i].copy(),
+            'gt_boxes': val.gt_boxes[batch_num + i].copy(),
+        }
+        assert np.all(objs_i[rels_i[:, 0]] > 0) and np.all(objs_i[rels_i[:, 1]] > 0)
+
+        pred_entry = {
+            'pred_boxes': boxes_i * BOX_SCALE/IM_SCALE,
+            'pred_classes': objs_i,
+            'pred_rel_inds': rels_i,
+            'obj_scores': obj_scores_i,
+            'rel_scores': pred_scores_i,  # hack for now.
+        }
+
+        eval_entry(conf.mode, gt_entry, pred_entry, evaluator, evaluator_multiple_preds,
+                   evaluator_list, evaluator_multiple_preds_list)
+
+def val_epoch():
+    detector.eval()
+    evaluator_list = [] # for calculating recall of each relationship except no relationship
+    evaluator_multiple_preds_list = []
+    for index, name in enumerate(ind_to_predicates):
+        if index == 0:
+            continue
+        evaluator_list.append((index, name, BasicSceneGraphEvaluator.all_modes()))
+        evaluator_multiple_preds_list.append((index, name, BasicSceneGraphEvaluator.all_modes(multiple_preds=True)))
+    evaluator = BasicSceneGraphEvaluator.all_modes() # for calculating recall
+    evaluator_multiple_preds = BasicSceneGraphEvaluator.all_modes(multiple_preds=True)
+
+    prog_bar = tqdm(enumerate(val_loader), total=int(len(val)/val_loader.batch_size))
+
+    with torch_no_grad():
+        for val_b, batch in prog_bar:
+            val_batch(conf.num_gpus * val_b, batch, evaluator, evaluator_multiple_preds, evaluator_list, evaluator_multiple_preds_list)
+
+    recall = evaluator[conf.mode].print_stats()
+    recall_mp = evaluator_multiple_preds[conf.mode].print_stats()
+
+    mean_recall = calculate_mR_from_evaluator_list(evaluator_list, conf.mode)
+    mean_recall_mp = calculate_mR_from_evaluator_list(evaluator_multiple_preds_list, conf.mode, multiple_preds=True)
+
+    detector.train()
+    return recall, recall_mp, mean_recall, mean_recall_mp
+
 ckpt = torch.load(conf.ckpt)    # 加载参数文件
 optimistic_restore(detector, ckpt['state_dict'], skip_clean=False)  # 参数导入模型中
 detector.cuda() # 模型移至 CUDA
 print(print_para(detector), flush=True) # 打印模型参数
 detector.eval() # 评估模式，禁用梯度记录
-recall, recall_mp, mean_recall, mean_recall_mp = _val_epoch(conf, detector, ind_to_predicates,
-                                                            val_loader, val, False)    # 开始评估
+recall, recall_mp, mean_recall, mean_recall_mp = val_epoch  # 开始评估
