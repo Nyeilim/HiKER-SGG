660804c4d87f0e94f91e361388f9c0f210d61b3f
Nyeilim <yan.jinghui@foxmail.com>
2024-09-22 16:31:20 +0800

    use simple summary function

--- run/hikersgg_predcls_test.py
+++ run/hikersgg_predcls_test.py
@@ -143,8 +143,7 @@ ckpt = torch.load(conf.ckpt)    # 加载参数文件
 optimistic_restore(detector, ckpt['state_dict'], skip_clean=False)  # 参数导入模型中
 detector.cuda() # 模型移至 CUDA
 print(print_para(detector), flush=True) # 打印模型参数
-one_sample = next(iter(val_loader))
-one_sample.scatter()
-summary(detector, input_data= [*one_sample[0]]) # 使用 torchinfo 打印模型信息
+summary(detector) # 使用 torchinfo 打印模型信息
+print(detector)
 detector.eval() # 评估模式，禁用梯度记录
 recall, recall_mp, mean_recall, mean_recall_mp = val_epoch()  # 开始评估
